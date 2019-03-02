67dc57066d8fc8ec71e111f1340bfecbc3a73286
Matt Dohlen <matt.dohlen@gmail.com>
2019-03-01 16:03:12 -0800

    Init Program

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:DartExamples/DartExamples.dart' as DartExamples;
+
+main(List<String> arguments) {
+  print('Hello world: ${DartExamples.calculate()}!');
+}
---
+++ test/DartExamples_test.dart
@@ -0,0 +1,9 @@
+import 'package:DartExamples/DartExamples.dart';
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
